f43a127ceea06913b3d887f05e54b29e195dd046
mengguanya <3052995873@qq.com>
2021-03-02 16:47:57 +0800

    Lab: syscall

--- user/user.h
+++ user/user.h
@@ -1,5 +1,6 @@
 struct stat;
 struct rtcdate;
+struct sysinfo;
 
 // system calls
 int fork(void);
@@ -24,6 +25,9 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+int trace(int);
+int sysinfo(struct sysinfo *);
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
@@ -40,3 +44,4 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
